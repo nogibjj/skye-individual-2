4de5bd3c7821ce1ff7f912a196db51ccb5045da8
skyeau9 <skye.augsorn@duke.edu>
2024-10-06 22:14:32 -0400

    comment delete existing log file code

--- mylib/log.py
+++ mylib/log.py
@@ -2,11 +2,11 @@ import logging
 import os
 
 file_path = "log/database_log.log"
-
-# Check if the file exists
-if os.path.exists(file_path):
-    os.remove(file_path)
-
+#
+# # Check if the file exists
+# if os.path.exists(file_path):
+#     os.remove(file_path)
+#
 
 def init_log(file_name=file_path, level=logging.NOTSET):
     logging.basicConfig(
